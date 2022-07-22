a48bd7f688a5be6abc562863dc2addd6e2d4c370
Sam Clark <sclark@bnl.gov>
2022-07-22 10:08:21 -0400

    First commit (empty package)

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "cad_reflective_cns"
+
+with open("requirements/production.txt") as f:
+    requirements = parse_requirements(f)
+
+dependencies = [
+    *[str(req.req) for req in requirements]
+]
+
+setup(
+    name=PACKAGE_NAME,
+    use_scm_version=dict(write_to=f"{PACKAGE_NAME}/version.py"),
+    packages=find_packages(),
+    install_requires=dependencies,
+    setup_requires=["setuptools_scm"],
+)
+
