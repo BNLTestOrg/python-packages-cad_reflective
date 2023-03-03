e8d3485b8947b2afb2d9e38b86cb41d38eb1b3b3
Sam Clark <sclark@bnl.gov>
2023-03-03 10:18:59 -0500

    Ready for release

--- cad_reflective/client.py
+++ cad_reflective/client.py
@@ -3,7 +3,7 @@ import os
 import threading
 from typing import List
 
-from cad_io.cns import CNSClient, CNSData, cns_lookup
+from cad_io.cns import CNSClient, CNSData, cnslookup
 
 from .data import *
 
@@ -15,15 +15,16 @@ class ReflectiveCNSClient(CNSClient):
     unpacker: CNSUnpacker
 
     def __init__(self, port=0, overrides=None):
-        host = os.getenv("REFCNSHOST", "csinject02")
+        host = os.getenv("REFCNSHOST", "csreflect01")
         super().__init__(host, 0x36666666, 5)
+        self.addpackers()
 
     def addpackers(self):
         self.packer = CNSPacker()
         self.unpacker = CNSUnpacker(b"")
 
-    def call_3(self, names: List[str], host: str, prog: int, vers: int, register_or_not: bool) -> List[int]:
-        arg = names, host, prog, vers, register_or_not
+    def call_3(self, names: List[str], host: str, prog: int, vers: int, fallback: bool, register_or_not: bool) -> List[int]:
+        arg = names, host, prog, vers, fallback, register_or_not
         return self.make_call(
             3, arg, self.packer.pack_mapping, lambda: self.unpacker.unpack_list(self.unpacker.unpack_uint)
         )
--- cad_reflective/data.py
+++ cad_reflective/data.py
@@ -1,4 +1,5 @@
 from cad_io import rpc
+from cad_io.cns import EntryType
 
 
 class CNSUnpacker(rpc.Unpacker):
@@ -10,24 +11,26 @@ class CNSUnpacker(rpc.Unpacker):
         host = self.unpack_string().decode()
         prog = self.unpack_uint()
         vers = self.unpack_uint()
+        fallback = self.unpack_bool()
         register_or_not = self.unpack_bool()
         names = [name.decode() for name in self.unpack_array(self.unpack_string)]
-        return names, host, prog, vers, register_or_not
+        return names, host, prog, vers, fallback, register_or_not
 
 
 class CNSPacker(rpc.Packer):
     def pack_mapping(self, data):
-        names, host, prog, vers, register_or_not = data
+        names, host, prog, vers, fallback, register_or_not = data
         # self.pack_string(name.encode("ascii"))
         self.pack_string(host.encode("ascii"))
         self.pack_uint(prog)
         self.pack_uint(vers)
+        self.pack_bool(fallback)
         self.pack_bool(register_or_not)
         self.pack_array([name.encode("ascii") for name in names], self.pack_string)
 
     def pack_generic(self, data):
         entryType, string2, entry, value, longA, longB, string3, string4, status = data
-        self.pack_enum(entryType)
+        self.pack_enum(EntryType[entryType])
         self.pack_string(entry.encode("ascii"))
         self.pack_string(value.encode("ascii"))
         self.pack_string(string2.encode("ascii"))
@@ -36,3 +39,14 @@ class CNSPacker(rpc.Packer):
         self.pack_uint(status)
         self.pack_string(string3.encode("ascii"))
         self.pack_string(string4.encode("ascii"))
+
+    def pack_nothing(self):
+        self.pack_enum(EntryType["NO_VALUE"])
+        self.pack_string("".encode("ascii"))
+        self.pack_string("".encode("ascii"))
+        self.pack_string("".encode("ascii"))
+        self.pack_uint(0)
+        self.pack_uint(0)
+        self.pack_uint(0)
+        self.pack_string("".encode("ascii"))
+        self.pack_string("".encode("ascii"))
